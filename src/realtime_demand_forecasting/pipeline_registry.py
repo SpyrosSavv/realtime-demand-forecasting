35377b588701fed65ce23f2718e539872eeeda54
SpyrosSav96 <spyros.sav96@gmail.com>
2026-05-06 22:17:58 +0300

    feat(pipeline): register training pipeline

--- src/realtime_demand_forecasting/pipeline_registry.py
+++ src/realtime_demand_forecasting/pipeline_registry.py
@@ -1,9 +1,12 @@
 from .pipelines.feature_eng import create_feature_eng_pipeline
+from .pipelines.training import create_training_pipeline
 from kedro.pipeline import Pipeline
 
 def register_pipelines() -> dict[str, Pipeline]:
     feature_eng_pipeline = create_feature_eng_pipeline()
+    training_pipeline = create_training_pipeline()
 
     return {
-        "__default__": feature_eng_pipeline
+        "__default__": feature_eng_pipeline + training_pipeline,
+        "training": feature_eng_pipeline + training_pipeline,
     }
